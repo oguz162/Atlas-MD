08af9f2ba57dca0726556e28e9249a989f08b25d
unique_hunter <77807213+oguz162@users.noreply.github.com>
2023-07-03 14:51:55 +0300

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "905516491871";   // You can replace this number with yours //
 }
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
+global.mongodb = process.env.MONGODB || "mongodb+srv://oguz162577:<oguz2516.>@cluster0.3qiww13.mongodb.net/?retryWrites=true&w=majority";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
