d621dacc4dbcccb037af980db477fd546e74d146
unique_hunter <77807213+oguz162@users.noreply.github.com>
2023-07-15 22:19:40 +0300

    Update core.js

--- Plugins/core.js
+++ Plugins/core.js
@@ -15,7 +15,7 @@ module.exports = {
   name: "others",
   alias: [...mergedCommands],
   uniquecommands: ["sc", "support"],
-  description: "All miscleaneous commands",
+  description: "Tüm Komutlar",
   start: async (Atlas, m, { pushName, prefix, inputCMD, doReact }) => {
     let pic = fs.readFileSync("./Assets/Atlas.jpg");
     switch (inputCMD) {
@@ -23,25 +23,25 @@ module.exports = {
       case "sc":
         await doReact("🧣");
         let repoInfo = await axios.get(
-          "https://api.github.com/repos/FantoX001/Atlas-MD"
+          "..."
         );
         let repo = repoInfo.data;
-        let txt = `            🧣 *${botName}'s Script* 🧣\n\n*🎀 Total Forks:* ${
+        let txt = `            🧣 *${botName}'s Script* 🧣\n\n*🎀 Toplam fork:* ${
           repo.forks_count
-        }\n*⭐ Total Stars:* ${repo.stargazers_count}\n*📜 License:* ${
+        }\n*⭐ Toplam yıldız${repo.stargazers_count}\n*📜 License:* ${
           repo.license.name
-        }\n*📁 Repo Size:* ${(repo.size / 1024).toFixed(
+        }\n*📁 Repo boyutu:* ${(repo.size / 1024).toFixed(
           2
-        )} MB\n*📅 Last Updated:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${
+        )} MB\n*📅 en son güncelleme:* ${repo.updated_at}\n\n*🔗 Repo Link:* ${
           repo.html_url
-        }\n\n❝ Dont forget to give a Star ⭐ to the repo. It's made with restless hardwork by *Team ATLAS*. ❞\n\n*©️ Team ATLAS- 2023*`;
+        }\n\n*©️ Alpha - 2023*`;
         Atlas.sendMessage(m.from, { image: pic, caption: txt }, { quoted: m });
         break;
 
       case "support":
       case "supportgc":
         await doReact("🔰");
-        let txt2 = `              🧣 *Support Group* 🧣\n\n*${botName}* is an open source project, and we are always happy to help you.\n\n*Link:* ${suppL}\n\n*Note:* Please don't spam in the group, and don't message *Admins directly* without permission. Ask for help inside *Group*.\n\n*Thanks for using Atlas.*`;
+        let txt2 = `     `;
         Atlas.sendMessage(m.from, { image: pic, caption: txt2 }, { quoted: m });
         break;
 
@@ -98,7 +98,7 @@ module.exports = {
 
         const allCommands = readUniqueCommands(pluginsDir);
         const formattedCommands = formatCommands(allCommands);
-        var helpText = `\nKonnichiwa *${pushName}* Senpai,\n\nI am *${botName}*, a WhatsApp bot built to take your boring WhatsApp experience into next level.\n\n*🔖 My Prefix is:*  ${prefix}\n\n${formattedCommands}\n\n\n*©️ Team ATLAS- 2023*`;
+        var helpText = `\nMerhaba *${pushName}* nasılsın,\n\nBen *${botName}*,statik nörol yapay ağlarla oluşturulmuş bir yapay zekayım ve hizmetinizdeyim.\n\n*🔖 önek:*  ${prefix}\n\n${formattedCommands}\n\n\n*©️ Alpha - 2023*`;
         await Atlas.sendMessage(
           m.from,
           { video: { url: botVideo }, gifPlayback: true, caption: helpText },
